cc2551a27cdbdb941453cc1137f715c55b6b2953
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-09-24 11:40:45 +0800

    update constructor arguments

--- deploy/10_deploy_SOLV.js
+++ deploy/10_deploy_SOLV.js
@@ -3,11 +3,15 @@ const colors = require('colors');
 module.exports = async ({ getNamedAccounts, deployments, network }) => {
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
+  const initHolders = {
+    'sepolia': deployer,
+    'mainnet': '0x8749b13fe36C72B745F948f91Fb2Ffec3B63bf75',
+  }
 
   const instance = await deploy('SOLV', {
     contract: 'SOLV',
     from: deployer,
-    args: [ deployer ],
+    args: [ initHolders[network.name] ],
     deterministicDeployment: ethers.encodeBytes32String("SOLV"),
     log: true,
   });
