ab4acddd66a53221431be3889414e158d040e7f4
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-11-24 19:40:10 +1100

    support hardcap

--- contracts/SOLV.sol
+++ contracts/SOLV.sol
@@ -3,12 +3,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract SOLV is ERC20 {
+contract SOLV is ERC20, Ownable2Step {
 
-    constructor(address initialHolder) ERC20("Solv", "SOLV") {
-        uint256 totalSupply = 8_400_000_000 * 1e18;
-        _mint(initialHolder, totalSupply);
+    uint256 public hardCap;
+    uint256 public constant INITIAL_SUPPLY = 8_400_000_000 * 1e18;
+
+    constructor(address initialHolder) ERC20("Solv", "SOLV") Ownable2Step(msg.sender) {
+        hardCap = INITIAL_SUPPLY * 2;
+        _mint(initialHolder, INITIAL_SUPPLY);
+    }
+
+    function mint(address to) external onlyOwner {
+        require(totalSupply() + amount <= hardCap, "Hard cap reached");
+        uint256 amount = INITIAL_SUPPLY * 5 / 100;
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external onlyOwner {
+        hardCap -= amount;
+        _burn(msg.sender, amount);
     }
 
 }
