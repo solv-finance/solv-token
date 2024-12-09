57a77a6228e606a38f2fbc12b0adf22a40eb585d
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-12-09 22:07:06 +1100

    support deploy on multiple chains

--- contracts/SOLV.sol
+++ contracts/SOLV.sol
@@ -8,14 +8,16 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 contract SOLV is ERC20, Ownable2Step {
 
     uint256 public accumulatedSupply;
-    uint256 public constant INITIAL_SUPPLY_PERCENT = 50; //50% of the initial supply
-    uint256 public constant INITIAL_SUPPLY = 8_400_000_000 * 1e18 * INITIAL_SUPPLY_PERCENT / 100;
-    uint256 public constant MAX_SUPPLY = INITIAL_SUPPLY * 2;
+    uint256 public constant MAX_SUPPLY = 8_400_000_000 * 1e18 * 2; //max supply on all chains
+    uint256 public constant MAX_SUPPLY_PER_CHAIN = MAX_SUPPLY / 2; //max supply per chain ( ethereum and bnb chain )
+    uint256 public constant INITIAL_SUPPLY_PERCENT = 50; //the initial supply is 50% of the max supply per chain
+    uint256 public constant INITIAL_SUPPLY = MAX_SUPPLY_PER_CHAIN * INITIAL_SUPPLY_PERCENT / 100; //initial supply per chain
+
     uint256 public constant MINT_CYCLE = 3 * 30 * 24 * 60 * 60;
     uint256 public lastMintTimestamp;
     address public broContractAddress;
     bool public allowInflation;
-    
+
     event BroContractAddressSet(address broContractAddress);
 
     constructor(address initialHolder) ERC20("Solv", "SOLV") Ownable(msg.sender) {
@@ -29,8 +31,8 @@ contract SOLV is ERC20, Ownable2Step {
         require(allowInflation, "Minting finished");
         require(broContractAddress != address(0), "Bro contract address not set");
         require(block.timestamp - lastMintTimestamp >= MINT_CYCLE, "Mint cycle not reached");
-        uint256 amount = INITIAL_SUPPLY * 5 / 100;
-        require(accumulatedSupply + amount <= MAX_SUPPLY, "Max supply reached");
+        uint256 amount = MAX_SUPPLY_PER_CHAIN * 5 / 100;
+        require(accumulatedSupply + amount <= MAX_SUPPLY_PER_CHAIN, "Max supply reached");
         accumulatedSupply += amount;
         _mint(broContractAddress, amount);
         lastMintTimestamp = block.timestamp;
